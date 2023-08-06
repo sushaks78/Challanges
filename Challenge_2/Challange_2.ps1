5c834ba37e275fc68a246ae35f47a5c8da805052
Sushant Shinde <sushaks78@outlook.com>
2023-08-06 11:43:22 +0530

    Challange Final Draft

---
+++ Challenge_2/Challange_2.ps1
@@ -0,0 +1,9 @@
+﻿$outputPath = "C:\test"
+New-Item -Path $outputPath -ItemType Directory -ErrorAction Ignore
+$vmname= Get-ComputerInfo | select csname
+$Proxy=New-object System.Net.WebProxy
+$WebSession=new-object Microsoft.PowerShell.Commands.WebRequestSession
+$WebSession.Proxy=$Proxy
+$challange_2=Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Proxy $Null -Uri "http://169.254.169.254/metadata/instance?api-version=2021-01-01" -WebSession $WebSession
+$challange_json=$challange_2 | ConvertTo-Json -Depth 6
+$challange_json | Out-File c:\test\metadata.json -Append
---
+++ Challenge_3/Challange_3.ps1
@@ -0,0 +1,40 @@
+﻿# Define a function to retrieve nested values from a hashtable
+function Get-ValueFromNestedObject {
+    param (
+        [hashtable] $obj,
+        [string] $key
+    )
+
+    # Split the provided key into individual keys
+    $keys = $key -split '/'
+    
+    # Start with the top-level hashtable
+    $value = $obj
+    
+    # Loop through each key in the key path
+    foreach ($k in $keys) {
+        # Check if the current key exists in the current hashtable
+        if ($value.ContainsKey($k)) {
+            # If the key exists, update the current value to the value associated with the current key
+            $value = $value[$k]
+        } else {
+            # If the key doesn't exist, return $null to indicate the path is invalid
+            return $null
+        }
+    }
+    
+    # Return the final value located at the specified key path
+    return $value
+}
+
+# Example 1
+$obj1 = @{ "a" = @{ "b" = @{ "c" = "d" } } }
+$key1 = "a/b/c"
+$result1 = Get-ValueFromNestedObject -obj $obj1 -key $key1
+Write-Host "Example 1 Result: $result1"
+
+# Example 2
+$obj2 = @{ "x" = @{ "y" = @{ "z" = "a" } } }
+$key2 = "x/y/z"
+$result2 = Get-ValueFromNestedObject -obj $obj2 -key $key2
+Write-Host "Example 2 Result: $result2"
